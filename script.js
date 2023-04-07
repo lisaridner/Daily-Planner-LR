ab846dc0bfadce377f48380d762346b403be7b8a
Lisa Ridner <125208782+lisaridner@users.noreply.github.com>
2023-04-07 13:50:58 -0400

    finished javascript code

--- script.js
+++ script.js
@@ -1,67 +1,66 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-
-  $(".saveBtn").on("click", function(){
-    console.log($(this).siblings("textarea").val())
-    var id= $(this).parent().attr("id")
-    var actualtext=$(this).siblings("textarea").val()
-    localStorage.setItem(id, actualtext)
-  })
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
-  var hours = {
-  9: document.getElementById("hour-9"),
-  10: document.getElementById("hour-10"),
-  11: document.getElementById("hour-10"),
-  12: document.getElementById("hour-10"),
-  13: document.getElementById("hour-10"),
-  14: document.getElementById("hour-10"),
-  15: document.getElementById("hour-10"),
-  16: document.getElementById("hour-10"),
-  17: document.getElementById("hour-10"),
-  }; 
-
-  var today = dayjs();
-  var time = today.format("H")
-// TODO: Add code to display the current date in the header of the page.
-  $("#currentDay").text(dayjs().format("MM/DD/YYYY"))
-
-  var plan=JSON.parse(localStorage.getItem("plan"))
-  $("textarea").innerText = plan
-
-  Object.keys(hours).forEach(function(key) {
-    var hour = hours[key];
-    
-  }
-
+var hours = {
+  9: document.getElementById('hour-9'),
+ 10: document.getElementById('hour-10'),
+ 11: document.getElementById('hour-11'),
+ 12: document.getElementById('hour-12'),
+ 13: document.getElementById('hour-13'),
+ 14: document.getElementById('hour-14'),
+ 15: document.getElementById('hour-15'),
+ 16: document.getElementById('hour-16'),
+ 17: document.getElementById('hour-17')
+ };
 
   
+ var today = dayjs();
+ var time = today.format('H')
+
+ $('#currentDay').text(today.format('dddd, MMMM D'));
+ 
   
    
-   // loop through each time block in the HTML, pull hour out and capture just the number, if statements and else statements
-  } 
-  colorHourBlock()
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-
-
+   var plan = JSON.parse(localStorage.getItem('plan'))
   
-  
-});
+  $('textarea').innerText = plan
+ 
+ 
+ Object.keys(hours).forEach(function(key){
+   var hour = hours[key];
+   var hourNumber = parseInt(key);
+   if (hourNumber < time){
+ hour.classList.add("past")
+   }else if (hourNumber <= time){
+     hour.classList.add("present")
+   }else{
+     hour.classList.add("future")
+   }
+ 
+ var plan = localStorage.getItem('plan-' + key);
+ if (plan){
+   $(hour).find('textarea').val(plan);
+ }
+ });
+ 
+ $('button').click(function(){
+  var textarea = $(this).siblings('textarea');
+   var key = 'plan-' + textarea.parent().attr('id').split('-')[1];
+   writtenPlan = textarea.val()
+   if (localStorage.getItem(key) === writtenPlan){
+      localStorage.removeItem(key);
+      textarea.val('')
+   }else{
+     localStorage.setItem(key,textarea.val());
+    
+     var respones = $ ('<p>', {
+       text: "your items were saved to local storage",
+       css:{
+         color: 'red'
+       }
+     })
+     $('#head').html(respones);
+   }
 
-localStorage.getItem(id, actualtext);
+   
+ });
+
+ 
